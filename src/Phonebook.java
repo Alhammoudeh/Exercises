24dc0c49a641bd0fb8019e3fe90f7e7c14a1544f
Alhammoudeh <Hammoudeh.alaa@gmail.com>
2017-11-12 20:51:16 -0800

    Update Phonebook.java

--- src/Phonebook.java
+++ src/Phonebook.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * You are given a phone book that consists of people's names and their phone number. 
  * After that you will be given some person's name as query. 
  * For each query, print the phone number of that person.
- * @author Alaa
+ * @author Al
  *
  */
 public class Phonebook {
