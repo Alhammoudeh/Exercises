40fc69602c57c950913cbb398c60e45ac2d2b6a4
Alhammoudeh <Hammoudeh.alaa@gmail.com>
2017-11-12 20:52:04 -0800

    Update JavaStringTokens.java

--- src/JavaStringTokens.java
+++ src/JavaStringTokens.java
@@ -5,7 +5,7 @@ import java.util.*;
  * Given a string, , matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. 
  * We define a token to be one or more consecutive English alphabetic letters. 
  * Then, print the number of tokens, followed by each token on a new line.
- * @author Alaa
+ * @author Al
  *
  */
 public class JavaStringTokens {
